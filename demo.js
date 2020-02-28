c0a1b076d2628535d6512b3551d53f4d8d7dca92
Julia1986 <59840160+Julia1986@users.noreply.github.com>
2020-02-28 21:15:08 +0200

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,56 @@
+$('#carousel').owlCarousel({
+    loop:true,
+    margin:10,
+	dots:false,
+	nav:true,
+	autoplay:true,
+	autoplayTimeout: 12000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('#carousel1').owlCarousel({
+    loop:true,
+    margin:10,
+	autoplay:true,
+	autoplayTimeout: 12000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('#carousel2').owlCarousel({
+    loop:true,
+    margin:10,
+	dots:true,
+	nav:false,
+	autoplay:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
